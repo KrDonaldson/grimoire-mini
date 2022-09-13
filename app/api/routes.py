c8ad1166ad61785025c25113e4bd83efdbc996ed
Kristina Donaldson <donaldsonkristinar@yahoo.com>
2022-09-13 00:35:02 -0600

    semi-final commit

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,36 +1,37 @@
 from flask import Blueprint, request, jsonify, render_template, redirect, url_for
-from helpers import token_required
 from models import db, User, Entry, entry_schema, entries_schema
 from flask_login import current_user
 from additemform import AddItemForm
 
 api = Blueprint('api',__name__, url_prefix='/journal')
 
+@api.route('/', methods = ['GET'])
+def get_entries():
+    a_user = current_user.id
+    entry = Entry.query.filter_by(user_id = a_user).all()
+    response = entries_schema.dump(entry)
+    return render_template('journal.html', entries = response)
+
 @api.route('/entry', methods=['POST', 'GET'])
-# @token_required
 def create_entry():
     form = AddItemForm()
     try:
         if request.method == 'POST':
-            print('you selected post')
             name = form.name.data
             details = form.details.data
             user_id = current_user.id
 
             entry = Entry(name=name, details=details, user_id=user_id)
-            print('entry is entered')
             db.session.add(entry)
             db.session.commit()
-            print('sent to db')
-            return redirect(url_for('auth.get_entry'))
+            return redirect(url_for('api.get_entries'))
     except:
         print('did not go through for whatever reason')
     return render_template('addEntry.html', form=form)
 
-@api.route('/', methods = ['GET'])
-# @token_required
-def get_entries():
-    a_user = current_user.id
-    entry = Entry.query.filter_by(user_id = a_user).all()
-    response = entries_schema.dump(entry)
-    return render_template('journal.html')
+@api.route('/remove/<id>', methods = ['DELETE'])
+def delete_entry(id):
+    entry = Entry.query.get(id)
+    db.session.delete(entry)
+    db.session.commit()
+    
